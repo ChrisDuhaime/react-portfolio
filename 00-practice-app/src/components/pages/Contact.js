7bd5cebaeb4fd5a13fe3430959e59c9be65d3fd8
Chris Duhaime <c.duhaime27@gmail.com>
2022-08-10 19:57:09 -0400

    added contact info

--- 00-practice-app/src/components/pages/Contact.js
+++ 00-practice-app/src/components/pages/Contact.js
@@ -5,8 +5,14 @@ export default function Contact() {
     <div>
       <h1>Contact Page</h1>
       <p>
-        Contact Page
+        Please feel free to contact me from the list below
       </p>
+       <ul>
+            <li>603-540-3135</li>
+            <li>C.duhaime27@gmail.com</li>
+            <li><a href="https://github.com/ChrisDuhaime">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/christopher-duhaime-a31b0745/">LinkedIn</a></li>
+        </ul>
     </div>
   );
 }
